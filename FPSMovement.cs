7df7048c42d396c68df35b1deda243063785a6d1
ZyanWasHere <59893921+ZyanWasHere@users.noreply.github.com>
2021-02-05 16:21:47 -0800

    Add files via upload

---
+++ FPSMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSMovement : MonoBehaviour
+{
+    //Controls player's movement
+    public float MovementSpeed;
+
+    //Updates inputs
+    void Update()
+    {
+        MovementUpdate();
+    }
+
+    //Updates player's movement, used in Update()
+    void MovementUpdate()
+    {
+        Vector3 PlayerMovement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        transform.Translate(PlayerMovement * MovementSpeed * Time.deltaTime);
+    }
+}
